374491346c30eef9a27d3ee8f0f84f0bb2bb1307
Jiaqi-beep <jiaqili1620@gmail.com>
2021-12-30 11:16:24 -0500

    local changes

--- Prework/ViewController.swift
+++ Prework/ViewController.swift
@@ -8,12 +8,36 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var billAmountTextField: UITextField!
+    @IBOutlet weak var tipAmountLabel: UILabel!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var amountPerPerson: UILabel!
+    @IBOutlet weak var partySize: UIStepper!
+    @IBOutlet weak var partySizeLabel: UILabel!
+    
     override func viewDidLoad() {
+        
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    
+    @IBAction func calculateTip(_ sender: Any) {
+        let bill = Double(billAmountTextField.text!) ?? 0
+        let tipPercentages = [0.15, 0.18, 0.2]
+        let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
+        let total = bill + tip
+        let per = total / partySize.value
+        
+        tipAmountLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+        amountPerPerson.text = String(format: "$%.2f", per)
+        partySizeLabel.text = String(format: "%.0f", partySize.value)
+    }
+    
+    
 }
 
